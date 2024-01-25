b022839772ec729f1337add089addba364de1ca6
Sudeep Saurabh <ssaurabh3@myseneca.ca>
2024-01-25 16:52:45 -0500

    Module for Network Part

---
+++ Terraform/module/network/variable.tf
@@ -0,0 +1,29 @@
+variable "default_tags" {
+  default = {}
+  type    = map(any)
+
+}
+
+
+# Name prefix
+variable "prefix" {
+  type = string
+}
+
+
+variable "env" {
+  type = string
+}
+
+
+variable "region" {
+  type = string
+}
+
+variable "vpc" {
+  type = map(string)
+}
+
+variable "public_cidr" {
+  type = map(string)
+}
